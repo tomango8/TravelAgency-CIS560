c6b35d71fa30ed80c3f8291a47c349c436fdbe1d
Ben Amos <bcamos@ksu.edu>
2019-11-16 11:02:20 -0600

    A Few Procedures

---
+++ SQL/Procedures/RestaurantsProcedures/Restaurants.GetRestaurantReservation.sql
@@ -0,0 +1,8 @@
+﻿CREATE OR ALTER PROCEDURE Restaurants.GetRestaurantReservation
+	@ReservationID INT
+AS
+
+SELECT ReservationID, ReservationDate, RestaurantID, ReservationTime
+FROM Restaurants.RestaurantReservation
+WHERE ReservationID = @ReservationID;
+GO
