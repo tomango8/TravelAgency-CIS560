8c2e48fb8cd1c5b0682c2df2d50b3992a1c11811
sidharth <sidharth@CIS-RDS4>
2019-12-01 23:27:56 -0600

    delegate created

---
+++ DataModeling/C#/DataDelegates/AttractionsDelegates/GetAttractionTickectDelegate.cs
@@ -0,0 +1,36 @@
+using DataAccess;
+using PersonData.Models;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DataModeling
+{
+    public class FetchPersonDataDelegate : DataReaderDelegate<Ticket>
+    {
+        private readonly int reservationID;
+
+        public FetchPersonDataDelegate(int reservationID)
+           : base("Attractions.GetAttractionTicket")
+        {
+            this.reservationID = reservationID;
+        }
+
+        public override void PrepareCommand(SqlCommand command)
+        {
+            base.PrepareCommand(command);
+
+            command.Parameters.AddWithValue("ReservationID", reservationID);
+        }
+
+        public override Person Translate(SqlCommand command, IDataRowReader reader)
+        {
+            if (!reader.Read())
+                throw new RecordNotFoundException(reservationID.ToString());
+
+            return new Person(reader.GetString('ReservationID'),
+               reader.GetString("AttractionID"),
+               reader.GetString("TicketDate"),
+               reader.GetString("Price"));
+        }
+    }
+}
