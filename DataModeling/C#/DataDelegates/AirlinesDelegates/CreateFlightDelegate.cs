e4ecc414c18d1b818b77f8bc910e48f327f71e74
Kevin Merwin <ksmerwin@ksu.edu>
2019-12-04 14:39:30 -0600

    progress on pages

---
+++ DataModeling/C#/DataDelegates/AirlinesDelegates/CreateFlightDelegate.cs
@@ -0,0 +1,46 @@
+﻿using DataAccess;
+using System.Data;
+using System.Data.SqlClient;
+using DataModeling.Model;
+
+namespace DataModeling
+{
+    public class CreateFlightDelegate : NonQueryDataDelegate<Flight>
+    {
+        public readonly int flightID;
+        public readonly string airlinename;
+        public readonly System.DateTime departuretime;
+        public readonly System.DateTime arrivaltime;
+        public readonly int citydepartureID;
+        public readonly int cityarrivalID;
+        public readonly System.DateTime date;
+
+        public CreateFlightDelegate(int FlightID)
+            : base("Airlines.CreateFlight")
+        {
+            this.flightID = FlightID;
+        }
+
+        public override void PrepareCommand(SqlCommand command)
+        {
+            base.PrepareCommand(command);
+
+            command.Parameters.AddWithValue("FlightID", flightID);
+            command.Parameters.AddWithValue("AirlineName", airlinename);
+            command.Parameters.AddWithValue("DepartureTime", departuretime);
+            command.Parameters.AddWithValue("ArrivalTime", arrivaltime);
+            command.Parameters.AddWithValue("CityDepartureID", citydepartureID);
+            command.Parameters.AddWithValue("CityArrivalID", cityarrivalID);
+            command.Parameters.AddWithValue("Date", date);
+
+            var p = command.Parameters.Add("FlightID", SqlDbType.Int);
+            p.Direction = ParameterDirection.Output;
+        }
+
+        public override Flight Translate(SqlCommand command)
+        {
+            return new Flight((int)command.Parameters["FlightID"].Value, airlinename, departuretime, arrivaltime, citydepartureID, cityarrivalID, date);
+        }
+
+    }
+}
---
+++ DataModeling/C#/DataDelegates/AirlinesDelegates/GetFlightDelegate.cs
@@ -0,0 +1,53 @@
+﻿using DataAccess;
+using System.Data;
+using System.Data.SqlClient;
+using DataModeling.Model;
+
+namespace DataModeling
+{
+    public class GetFlightDelegate : DataReaderDelegate<Flight>
+    {
+        private readonly int FlightID;
+        public readonly string AirlineName;
+        public readonly System.DateTime DepartureTime;
+        public readonly int CityDepartureID;
+        public readonly System.DateTime ArrivalTime;
+        public readonly int CityArrivalID;
+        
+
+        public GetFlightDelegate(string airlineName, System.DateTime departureTime, int departureCityID, System.DateTime arrivalTime, int arrivalCityID)
+           : base("Location.RetrieveFlightInfo")
+        {
+            this.AirlineName = airlineName;
+            this.DepartureTime = departureTime;
+            this.CityDepartureID = departureCityID;
+            this.ArrivalTime = arrivalTime;
+            this.CityArrivalID = arrivalCityID;
+        }
+
+        public override void PrepareCommand(SqlCommand command)
+        {
+            base.PrepareCommand(command);
+            command.Parameters.AddWithValue("AirlineName", AirlineName);
+            command.Parameters.AddWithValue("DepartureTime", DepartureTime);
+            command.Parameters.AddWithValue("ArrivalTime", ArrivalTime);
+            command.Parameters.AddWithValue("CityDepartureID", CityDepartureID);
+            command.Parameters.AddWithValue("CityArrivalID", CityArrivalID);
+
+        }
+
+        public override Flight Translate(SqlCommand command, IDataRowReader reader)
+        {
+            if (!reader.Read())
+                return null;
+
+            return new Flight(FlightID,
+               reader.GetString("AirlineName"),
+               reader.GetDateTime("DepartureTime"),
+               reader.GetDateTime("ArrivalTime"),
+               reader.GetInt32("CityDepatureID"),
+               reader.GetInt32("CityArrivalID"),
+               reader.GetDateTime("Date"));
+        }
+    }
+}
