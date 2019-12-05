be7cae0cec9fbeb3c51d76d3873d840aabbc6b34
Ben Amos <bcamos@ksu.edu>
2019-12-04 19:37:06 -0600

    Some AgentsData

---
+++ DataModeling/SQL/Data/AgentsData.sql
@@ -0,0 +1,28 @@
+--Agents Data--
+USE TravelAgency;
+GO
+
+INSERT Agency.Agents([Name], Salary) 
+VALUES ('Ingrid Welch', 60000),
+('Lila Mitchell', 63000),('Minerva Dudley', 65000),
+('Elijah Lewis', 40000),('Forrest Kerr', 20000),
+('Jolie Barnes',30000),('Marsden Bradley', 25000),
+('Hayes Terry', 45000),('Evangeline Hughes', 32000),
+('Adria Hensley',75000),('Constance Duncan', 50500),
+('Branden Fuller', 50000),('Mallory Whitaker', 24000),
+('Gage Merritt', 64000),('Zachery Weiss', 45000),
+('Jasmine Vincent', 43000),('Odette Chan', 33000),
+('Vivian Talley', 60000),('Yvette Carrillo', 35000),
+('Jena Cannon', 47000),('Maia Garrison', 56000),
+('Brynne Mack', 57000),('Kibo Hunt', 76000),
+('Miriam Steele', 53000),('Aubrey Walters', 34000),
+('Sawyer Kirkland', 55000),('Maryam Mack', 34000),
+('Avye Bates', 36000),('Honorato Nolan', 56000),
+('Stacy Rhodes', 24000),('Silas Gaines', 47000),
+('Aspen Macdonald', 67000),('Sophia Henson', 54000),
+('Neil Raymond', 39000),('Shaeleigh Randolph', 29000),
+('Taylor Ochoa', 55000),('Gray Rowe', 78000),
+('Dennis Greene', 64000),('Kyle Mills', 36000),
+('Rana Barton', 54000),('Brock Gray', 62000),
+('Ahmed Owens', 36000),('Maggie Buckley', 32000),
+('Melinda Blevins', 43000),('Lee Hansen', 21000);
