5fbf9f44314bd3a66cb49b0da9b10b09fdd05c99
sidharth <sidharth@CIS-RDS4>
2019-12-06 00:19:04 -0600

    AgencyCarRentalReservationDelegate created

---
+++ DataModeling/C#/DataDelegates/AgencyProcedures/AgencyCarRentalReservationDelegate.cs
@@ -0,0 +1,48 @@
+﻿using DataAccess;
+using DataModeling.Model;
+using System.Collections.Generic;
+using System.Data;
+using System;
+using System.Data.SqlClient;
+
+namespace DataModeling
+{
+    public class AgencyCarRentalReservationDelegate : NonQueryDataDelegate<CarRentalReservation>
+    {
+        private int reservationID;
+        private int carRentalID ;
+        private DateTime rentalDate;
+        private string model;
+        private float price;
+
+        public AgencyCarRentalReservationDelegate(int reservationID, int carRentalID, DateTime rentalDate,
+                                          string model, float price)
+            : base(" Agency.CreateCarRentalReservation")
+        {
+            this.reservationID = reservationID;
+            this.carRentalID =carRentalID;
+            this.rentalDate = rentalDate;
+            this.model= model;
+            this.price = price;
+    }
+
+        public override void PrepareCommand(SqlCommand command)
+        {
+            base.PrepareCommand(command);
+
+            command.Parameters.AddWithValue("ReservationID", reservationID);
+            command.Parameters.AddWithValue("CarRentalID", carRentalID);
+            command.Parameters.AddWithValue("RentalDate", rentalDate);
+            command.Parameters.AddWithValue("Model", model);
+            command.Parameters.AddWithValue("Price", price);
+
+        }
+
+        public override CarRentalReservation Translate(SqlCommand command)
+        {
+            return new CarRentalReservation((int)reservationID, carRentalID, rentalDate,model,price);
+        }
+    }
+
+}
+
