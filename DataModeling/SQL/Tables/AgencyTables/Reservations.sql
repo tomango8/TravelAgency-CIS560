e855e4456b7d3e8e4972b8ae001a0cf84dccf803
Ben Amos <bcamos@ksu.edu>
2019-12-05 22:07:50 -0600

    BoardingPass Data

--- DataModeling/SQL/Tables/AgencyTables/Reservations.sql
+++ DataModeling/SQL/Tables/AgencyTables/Reservations.sql
@@ -3,11 +3,11 @@
 	ReservationID INT NOT NULL IDENTITY(1, 1) PRIMARY KEY,
 	DataCreated DATETIMEOFFSET NOT NULL DEFAULT(SYSDATETIMEOFFSET()),
 	IsDeleted BIT NOT NULL DEFAULT(0),
-	CarReservation BIT NOT NULL,
-	HotelReservation BIT NOT NULL,
-	BoardingPass BIT NOT NULL,
-	AttractionTicket BIT NOT NULL,
-	RestaurantReservation BIT NOT NULL,
+	CarReservation BIT NOT NULL DEFAULT(0),
+	HotelReservation BIT NOT NULL DEFAULT(0),
+	BoardingPass BIT NOT NULL DEFAULT(0),
+	AttractionTicket BIT NOT NULL DEFAULT(0),
+	RestaurantReservation BIT NOT NULL DEFAULT(0),
 	TripID INT FOREIGN KEY
 	REFERENCES Agency.Trips(TripID),
 	CONSTRAINT CHECK_RESERVATION_TYPE CHECK((CarReservation = 0 OR (HotelReservation = 0 AND BoardingPass = 0 AND AttractionTicket = 0 AND RestaurantReservation = 0)) 
