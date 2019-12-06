d30b269b0b089ad15a8c680e339568f05cb6c2c6
Ben Amos <bcamos@ksu.edu>
2019-12-06 12:37:00 -0600

    Fixed bug on plan trip screen

--- DataModeling/SQL/Procedures/AgencyProcedures/Agency.GetReservations.sql
+++ DataModeling/SQL/Procedures/AgencyProcedures/Agency.GetReservations.sql
@@ -5,5 +5,6 @@ SELECT R.ReservationID, R.DateCreated,
 R.CarReservation, R.HotelReservation, R.BoardingPass, R.AttractionTicket, R.RestaurantReservation,
 R.TripID
 FROM Agency.Reservations R
-WHERE IsDeleted = 0;
+WHERE R.IsDeleted = 0 AND
+@TripID = R.TripID;
 GO
