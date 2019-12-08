de8a951df794fb4e2a7dfc646206bbe8efeba6a2
Thomas Paul <tomango8@ksu.edu>
2019-12-08 16:12:08 -0600

    real report query 2

--- DataModeling/SQL/Reports/Agency.TopTenAttractions.sql
+++ DataModeling/SQL/Reports/Agency.TopTenAttractions.sql
@@ -1,18 +1,19 @@
 ﻿USE TravelAgency;
 GO
 CREATE OR ALTER PROC Agency.TopTenAttractions AS
-WITH attraction AS 
+WITH attraction(AttractionID, Customers, CityName, Country, Price) AS 
 (
-SELECT A.AttractionID, COUNT(T.CustomerID) AS Customers, C.CityName, C.Country 
-FROM Attractions.Attraction A
-	INNER JOIN Attractions.AttractionTicket [AT] ON AT.AttractionID = A.AttractionID
-	INNER JOIN [Location].Cities C ON C.CityID = A.CityID
+SELECT A.AttractionID, COUNT(T.CustomerID) AS Customers, C.CityName, C.Country, [AT].Price 
+FROM [Location].Cities C
+	INNER JOIN Attractions.Attraction A ON A.CityID = C.CityID
+	INNER JOIN Attractions.AttractionTicket [AT] ON A.AttractionID = [AT].AttractionID
 	INNER JOIN Agency.Reservations R ON R.ReservationID = [AT].ReservationID
 	INNER JOIN Agency.Trips T ON T.TripID = R.TripID
-	INNER JOIN Hotels.HotelReservation HR ON HR.ReservationID = R.ReservationID
-	INNER JOIN Hotels.Hotel H ON HR.HotelID = H.HotelID
-GROUP BY A.AttractionID, COUNT(T.CustomerID), C.CityName, C.Country
+	
+
+GROUP BY A.AttractionID, C.CityName, C.Country, [AT].Price
 )
-SELECT TOP 10 AttractionID, CityName, Country
+SELECT TOP 10 A.CityName, A.Country, A.Price
 FROM attraction A
-ORDER BY Customers DESC;
+ORDER BY A.Customers DESC;
+
